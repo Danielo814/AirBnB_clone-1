2e5b65965aa3659e97998d2209534cdf2ba728c0
Paul Lave <pmlave@gmail.com>
2019-01-08 23:17:51 +0000

    Updated to account for environment variable

--- models/__init__.py
+++ models/__init__.py
@@ -8,9 +8,10 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+import os
 
 
-if HBNB_TYPE_STORAGE == "db":
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
     storage = DBStorage()
 else:
     storage = FileStorage()
