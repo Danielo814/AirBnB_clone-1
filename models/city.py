8b2aedafeb3a2e3ba304e28d3dbf64906d3fb142
Daniel Ojeda <211@holbertonschool.com>
2019-01-09 01:36:52 +0000

    add changes to storage

--- models/city.py
+++ models/city.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This is the city class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 
@@ -14,4 +14,4 @@ class City(BaseModel, Base):
     """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), nullable=False, ForeignKey("states.id"))
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
--- models/state.py
+++ models/state.py
@@ -2,26 +2,30 @@
 """This is the state class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+import os
 
 class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete-orphan", backref="state")
 
-    """File Storage"""
-    @property
-    def cities(self):
-        """
-        city properties
-        """
-        cls = []
-        for val in storage.all("City").values():
-            if val.state_id == self.id:
-                cls.append(val)
-        return cls
+    else:
+
+        """File Storage"""
+        @property
+        def cities(self):
+            """
+            city properties
+            """
+            cls = []
+            for val in storage.all(City).values():
+                if val.state_id == self.id:
+                    cls.append(val)
+            return cls
