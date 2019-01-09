c20bca54fd96c88141e4c8a1286aefd09e7a199b
Daniel Ojeda <211@holbertonschool.com>
2019-01-09 22:07:09 +0000

    push changes to city and place

--- models/place.py
+++ models/place.py
@@ -11,7 +11,7 @@ place_amenity = Table('place_amenity', Base.metadata,
                              primary_key=True, nullable=False),
                       Column('amenity_id', String(60),
                              ForeignKey('amenities.id'),
-                             primary_key=True, nullable=False))
+                             primary_key=True, nullable=True))
 
 class Place(BaseModel, Base):
     """This is the class for Place
@@ -31,9 +31,9 @@ class Place(BaseModel, Base):
 
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
+    description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
@@ -66,7 +66,7 @@ class Place(BaseModel, Base):
         Amenities attributes
         """
         cls = []
-        for val in models.storage.all('Amenity').values():
+        for val in models.storage.all(Amenity).values():
             if amenity_ids == self.id:
                 cls.append(val)
         return cls
