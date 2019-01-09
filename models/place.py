c579a03869ffeb9fae0d406be50561c12cd3bff6
ceecurvin <ccurvi1@lsu.edu>
2019-01-09 19:46:15 +0000

    Minor formatting changes

--- models/place.py
+++ models/place.py
@@ -2,9 +2,10 @@
 """This is the place class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 
-class Place(BaseModel, Base ):
+
+class Place(BaseModel, Base):
     """This is the class for Place
     Attributes:
         city_id: city id
@@ -47,13 +48,17 @@ class Place(BaseModel, Base ):
                 cls.append(val)
         return cls
 
-
     place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60), nullable=False)
-                          Column('amenity_id', String(60), nullable=False))
+                          Column('place_id', String(60),
+                                 nullable=False, ForeignKey('places.id'),
+                                 primary_key=True)
+                          Column('amenity_id', String(60),
+                                 nullable=False,
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True))
 
-
-    amenities = relationship('Amenity', secondary=place_amenity, viewonly=False)
+    amenities = relationship('Amenity', secondary=place_amenity,
+                             viewonly=False)
 
     @property
     def amenities(self):
